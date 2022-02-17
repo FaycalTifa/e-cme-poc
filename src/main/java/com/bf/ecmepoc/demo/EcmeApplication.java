37c7a4fac7a4ef13c2bf9971fff30a93794cbd96
Afriklonnya <manitiabondou@gmail.com>
2022-02-17 19:46:09 +0100

    first commit

---
+++ src/main/java/com/bf/ecmepoc/demo/EcmeApplication.java
@@ -0,0 +1,13 @@
+package com.bf.ecmepoc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bf/ecmepoc/demo/EcmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bf.ecmepoc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
